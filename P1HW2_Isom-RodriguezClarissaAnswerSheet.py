189c4dfc66a68ace1da48d9db1548e3f994eec60
isomrodc6910 <95447076+isomrodc6910@users.noreply.github.com>
2022-07-18 21:19:47 -0400

    Add files via upload

---
+++ P1HW2_Isom-RodriguezClarissaAnswerSheet.py
@@ -0,0 +1,11 @@
+Python 3.9.7 (tags/v3.9.7:1016ef3, Aug 30 2021, 20:19:38) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/Clarissa Rodriguez/Documents/Web, Pgm, and Database/P1HW2_Isom-RodriguezClarissa.py
+Enter name of expense: Cable
+Enter monthly charge: 110
+Bill: Cable --------- Before Tax: $110.00
+Monthly Tax: $6.60
+Monthly Charge: $116.60
+Annual Charge: $1399.20
+>>> 
